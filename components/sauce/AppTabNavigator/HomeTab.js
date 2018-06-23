20b3ec0584cf8f55426d8f1f0dac054981ad5c95
MAinulYaqin <ainul.y9b@gmail.com>
2018-06-23 17:22:28 +0700

    adding header to profile tab

--- components/sauce/AppTabNavigator/HomeTab.js
+++ components/sauce/AppTabNavigator/HomeTab.js
@@ -1,84 +1,124 @@
-import React, {Component} from 'react';
-import {View, Text, StyleSheet, Platform, StatusBar, ScrollView} from 'react-native';
-import {Icon, Container, Content, Right, Left, Thumbnail, Header, Title, Body} from 'native-base';
+import React, { Component } from 'react';
+import {
+	View,
+	Text,
+	StyleSheet,
+	Platform,
+	StatusBar,
+	ScrollView
+} from 'react-native';
+import {
+	Icon,
+	Container,
+	Content,
+	Right,
+	Left,
+	Thumbnail,
+	Header,
+	Title,
+	Body
+} from 'native-base';
 
 // import components
 import CardComponent from '../CardComponent';
 
 export default class HomeTab extends Component {
-    static navigationOptions = {
-        tabBarIcon: ({ tintColor }) => (
-            <Icon name="home" style={{ color: tintColor }} />
-        )
-    }
+	static navigationOptions = {
+		tabBarIcon: ({ tintColor }) => (
+			<Icon name="home" style={{ color: tintColor }} />
+		)
+	};
 
-    render() {
-        const stories = [1, 2, 3, 4, 5, 6]
+	render() {
+		const stories = [1, 2, 3, 4, 5, 6];
 
-        return (
-            <Container>
+		return (
+			<Container>
+				<Header style={styles.androidHeader}>
+					<Left>
+						<Icon name="camera" style={{ paddingLeft: 10 }} />
+					</Left>
+					<Body style={styles.androidTitle}>
+						<Text
+							style={{
+								color: 'black',
+								fontFamily: 'Billabong',
+								fontWeight: '300',
+								fontSize: 27
+							}}>
+							Yaudin
+						</Text>
+					</Body>
+					<Right>
+						<Icon name="paper-plane" style={{ paddingRight: 10 }} />
+					</Right>
+				</Header>
 
-                <Header style={styles.androidHeader}>
-                     <Left><Icon name="camera" style={{paddingLeft: 10}}/></Left>
-                    <Body style={styles.androidTitle}>
-                        <Text style={{color: 'black', fontFamily: 'Billabong', fontWeight: '300', fontSize: 27}}>Yaudin</Text>
-                    </Body>
-                    <Right><Icon name="paper-plane" style={{paddingRight: 10}}/></Right>
-                </Header>
+				<Content>
+					<View>
+						<View
+							style={{
+								flex: 1,
+								flexDirection: 'row',
+								justifyContent: 'space-between',
+								alignItems: 'center',
+								paddingHorizontal: 7,
+								marginTop: 4
+							}}>
+							<Text style={{ fontWeight: 'bold' }}>Stories</Text>
 
-                <Content>
-                    <View>
-                        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingHorizontal: 7, marginTop: 4}}>
-                            <Text style={{ fontWeight: 'bold' }}>Stories</Text>
-
-                            <View style={{ flexDirection: 'row', 'alignItems': 'center' }}>
-                                <Icon name="play" style={{ fontSize: 14 }}></Icon>
-                                <Text style={{ fontWeight: 'bold' }}> Watch All</Text>
-                            </View>
-                        </View>
-                    </View>
-                    <View>
-                        <ScrollView horizontal={true}
-                            showsHorizontalScrollIndicator={false}
-                            contentContainerStyle={{
-                                alignItems: 'center',
-                                paddingStart: 5,
-                                paddingEnd: 5
-                            }}>
-                            {stories.map(e => (
-                                <Thumbnail style={styles.stories} key={e} source={require('../../../assets/feeder/feed-3.png')}/>
-                            ))}
-                        </ScrollView>
-                    </View>
-                    <CardComponent imageSource="2" likes="20"/>
-                    <CardComponent imageSource="3" likes="103"/>
-                </Content>
-            </Container>
-        )
-    }
+							<View style={{ flexDirection: 'row', alignItems: 'center' }}>
+								<Icon name="play" style={{ fontSize: 14 }} />
+								<Text style={{ fontWeight: 'bold' }}> Watch All</Text>
+							</View>
+						</View>
+					</View>
+					<View>
+						<ScrollView
+							horizontal={true}
+							showsHorizontalScrollIndicator={false}
+							contentContainerStyle={{
+								alignItems: 'center',
+								paddingStart: 5,
+								paddingEnd: 5
+							}}>
+							{stories.map(e => (
+								<Thumbnail
+									style={styles.stories}
+									key={e}
+									source={require('../../../assets/feeder/feed-3.png')}
+								/>
+							))}
+						</ScrollView>
+					</View>
+					<CardComponent imageSource="2" likes="20" />
+					<CardComponent imageSource="3" likes="103" />
+				</Content>
+			</Container>
+		);
+	}
 }
 
-
 const styles = StyleSheet.create({
-    androidHeader: {
-        ...Platform.select({
-            android: {
-                marginTop: StatusBar.currentHeight,
-            }
-        }),
-        backgroundColor: 'white'
-    },
-    androidTitle: {
-        ...Platform.select({
-            android: {
-                alignItems: 'flex-end'
-            }
-        }),
-        marginTop: 10
-    },
-    stories: {
-        marginHorizontal: 5,
-        borderColor: 'pink',
-        borderWidth: 2
-    }
+	androidHeader: {
+		...Platform.select({
+			android: {
+				marginTop: StatusBar.currentHeight,
+				backgroundColor: 'white'
+			}
+		}),
+	},
+	androidTitle: {
+		...Platform.select({
+			android: {
+				alignItems: 'flex-end',
+				marginTop: 10
+			}
+		})
+	},
+	stories: {
+		marginHorizontal: 5,
+		borderColor: 'pink',
+		borderWidth: 2
+	}
 });
--- components/sauce/AppTabNavigator/ProfileTab.js
+++ components/sauce/AppTabNavigator/ProfileTab.js
@@ -1,29 +1,71 @@
-import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-import {Icon} from 'native-base';
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Platform, StatusBar } from 'react-native';
+import {
+  Icon,
+  Container,
+  Header,
+  Content,
+  Body,
+  Left,
+  Right,
+  Title,
+  Button
+} from 'native-base';
 
 export default class ProfileTab extends Component {
+  static navigationOptions = {
+    tabBarIcon: ({ tintColor }) => (
+      <Icon name="person" style={{ color: tintColor }} />
+    )
+  };
 
-    static navigationOptions = {
-        tabBarIcon: ({ tintColor }) => (
-            <Icon name="person" style={{ color: tintColor }} />
-        )
-    }
-
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text>Profile tab section</Text>
-            </View>
-        )
-    }
+  render() {
+    return (
+      <Container>
+        <Header style={styles.androidHeader}>
+          <Left>
+            <Icon name="camera" style={{ paddingLeft: 10 }} />
+          </Left>
+          <Body style={styles.androidTitle}>
+            <Text
+              style={{
+                color: 'black',
+                fontFamily: 'Billabong',
+                fontWeight: '300',
+                fontSize: 27
+              }}>
+              Yaudin
+            </Text>
+          </Body>
+          <Right>
+            <Icon name="paper-plane" style={{ paddingRight: 10 }} />
+          </Right>
+        </Header>
+      </Container>
+    );
+  }
 }
 
-
 const styles = StyleSheet.create({
-    container: {
-        'flex': 1,
-        'alignItems': 'center',
-        'justifyContent': 'center',
-    }
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  androidHeader: {
+    ...Platform.select({
+      android: {
+        marginTop: StatusBar.currentHeight,
+        backgroundColor: 'white'
+      }
+    })
+  },
+  androidTitle: {
+    ...Platform.select({
+      android: {
+        alignItems: 'flex-end',
+        marginTop: 10
+      }
+    })
+  }
 });
