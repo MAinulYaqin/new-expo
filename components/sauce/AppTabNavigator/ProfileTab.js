10be312e8a8eb92ab3ae4e94658accd3a9522b68
MAinulYaqin <ainul.y9b@gmail.com>
2018-06-24 16:49:22 +0700

    updated header for profileTab

--- components/sauce/AppTabNavigator/ProfileTab.js
+++ components/sauce/AppTabNavigator/ProfileTab.js
@@ -1,71 +1,70 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, Platform, StatusBar } from 'react-native';
+import EntypoIcon from 'react-native-vector-icons/Entypo';
 import {
-  Icon,
-  Container,
-  Header,
-  Content,
-  Body,
-  Left,
-  Right,
-  Title,
-  Button
+	Icon,
+	Container,
+	Header,
+	Content,
+	Body,
+	Left,
+	Right,
+	Title,
+	Button
 } from 'native-base';
 
 export default class ProfileTab extends Component {
-  static navigationOptions = {
-    tabBarIcon: ({ tintColor }) => (
-      <Icon name="person" style={{ color: tintColor }} />
-    )
-  };
+	static navigationOptions = {
+		tabBarIcon: ({ tintColor }) => (
+			<Icon name="person" style={{ color: tintColor }} />
+		)
+	};
 
-  render() {
-    return (
-      <Container>
-        <Header style={styles.androidHeader}>
-          <Left>
-            <Icon name="camera" style={{ paddingLeft: 10 }} />
-          </Left>
-          <Body style={styles.androidTitle}>
-            <Text
-              style={{
-                color: 'black',
-                fontFamily: 'Billabong',
-                fontWeight: '300',
-                fontSize: 27
-              }}>
-              Yaudin
-            </Text>
-          </Body>
-          <Right>
-            <Icon name="paper-plane" style={{ paddingRight: 10 }} />
-          </Right>
-        </Header>
-      </Container>
-    );
-  }
+	render() {
+		return (
+			<Container>
+				{/* Navbar */}
+				<Header style={styles.androidHeader}>
+					<Left>
+						<Icon name="person-add" style={{ paddingLeft: 10 }} />
+					</Left>
+					<Body style={styles.androidTitle}>
+						<Text>AinulBedjo</Text>
+					</Body>
+					<Right>
+						<EntypoIcon
+							name="back-in-time"
+							style={{ paddingRight: 10, fontSize: 30 }}
+						/>
+					</Right>
+				</Header>
+				{/* Content */}
+				<Content />
+			</Container>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  androidHeader: {
-    ...Platform.select({
-      android: {
-        marginTop: StatusBar.currentHeight,
-        backgroundColor: 'white'
-      }
-    })
-  },
-  androidTitle: {
-    ...Platform.select({
-      android: {
-        alignItems: 'flex-end',
-        marginTop: 10
-      }
-    })
-  }
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	androidHeader: {
+		...Platform.select({
+			android: {
+				marginTop: StatusBar.currentHeight,
+				backgroundColor: 'white'
+			}
+		})
+	},
+	androidTitle: {
+		...Platform.select({
+			android: {
+				alignItems: 'flex-end',
+				marginTop: 10
+			}
+		})
+	}
 });
