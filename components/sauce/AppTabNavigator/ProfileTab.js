0bb25d5c6471fc6ad2cb9edc288ca61817a6abc3
MAinulYaqin <ainul.y9b@gmail.com>
2018-07-01 19:58:47 +0700

    fixing header and add some styling

--- components/sauce/AppTabNavigator/ProfileTab.js
+++ components/sauce/AppTabNavigator/ProfileTab.js
@@ -1,30 +1,11 @@
 import React, { Component } from 'react';
-import {
-	View,
-	Text,
-	StyleSheet,
-	Platform,
-	StatusBar,
-	Image
-} from 'react-native';
+import { View, Text, StyleSheet, Platform, StatusBar, Image } from 'react-native';
 import EntypoIcon from 'react-native-vector-icons/Entypo';
-import {
-	Icon,
-	Container,
-	Header,
-	Content,
-	Body,
-	Left,
-	Right,
-	Title,
-	Button
-} from 'native-base';
+import { Icon, Container, Header, Content, Body, Left, Right, Title, Button } from 'native-base';
 
 export default class ProfileTab extends Component {
 	static navigationOptions = {
-		tabBarIcon: ({ tintColor }) => (
-			<Icon name="person" style={{ color: tintColor }} />
-		)
+		tabBarIcon: ({ tintColor }) => <Icon name="person" style={{ color: tintColor }} />
 	};
 
 	render() {
@@ -44,14 +25,11 @@ export default class ProfileTab extends Component {
 						</Text>
 					</Body>
 					<Right>
-						<EntypoIcon
-							name="back-in-time"
-							style={{ paddingRight: 10, fontSize: 27 }}
-						/>
+						<EntypoIcon name="back-in-time" style={{ paddingRight: 10, fontSize: 27 }} />
 					</Right>
 				</Header>
 				{/* Content */}
-				<Content>
+				<Content style={{ padding: 10 }}>
 					<View
 						style={{
 							flexDirection: 'row'
@@ -67,26 +45,27 @@ export default class ProfileTab extends Component {
 							/>
 						</View>
 						<View style={{ flex: 3, flexDirection: 'column' }}>
+							{/* posts followers and following */}
 							<View
 								style={{
 									flexDirection: 'row',
 									justifyContent: 'space-around'
 								}}>
 								<View style={{ alignItems: 'center' }}>
-									<Text>20</Text>
-									<Text>posts</Text>
+									<Text style={styles.boldNum}>20</Text>
+									<Text style={styles.textTop}>posts</Text>
 								</View>
 								<View style={{ alignItems: 'center' }}>
-									<Text>320</Text>
-									<Text>followers</Text>
+									<Text style={styles.boldNum}>320</Text>
+									<Text style={styles.textTop}>followers</Text>
 								</View>
 								<View style={{ alignItems: 'center' }}>
-									<Text>32</Text>
-									<Text>following</Text>
+									<Text style={styles.boldNum}>32</Text>
+									<Text style={styles.textTop}>following</Text>
 								</View>
 							</View>
-
-							<View style={{ flexDirection: 'row' }}>
+							{/* Button edit and setting */}
+							<View style={{ flexDirection: 'row', marginTop: 10 }}>
 								<Button
 									bordered
 									dark
@@ -102,11 +81,23 @@ export default class ProfileTab extends Component {
 								<Button
 									bordered
 									dark
-									style={{ justifyContent: 'center', height: 30, flex: 1, marginLeft: 10, marginRight: 5 }}>
+									style={{
+										justifyContent: 'center',
+										height: 30,
+										flex: 1,
+										marginLeft: 10,
+										marginRight: 5
+									}}>
 									<Icon name="settings" />
 								</Button>
 							</View>
 						</View>
+					</View>
+					<View>
+						<Text>
+							lorem ipsum dolor sit amet. lorem ipsum dolor sit amet. lorem ipsum dolor sit amet.
+							lorem ipsum dolor sit amet. lorem ipsum dolor sit amet.
+						</Text>
 					</View>
 				</Content>
 			</Container>
@@ -134,5 +125,11 @@ const styles = StyleSheet.create({
 				alignItems: 'flex-end'
 			}
 		})
+	},
+	boldNum: {
+		fontWeight: '700'
+	},
+	textTop: {
+		marginTop: -5
 	}
-})
+});
